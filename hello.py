610f39b7a2162a68ef41c826221974e63dbc229c
MORAI-JunHong <junedear@morai.ai>
2018-11-18 17:47:12 +0900

    Revert "Revert "Revert "GitHub for git pull"""
    This reverts commit 6c8c21e68ba88d77725b7bc6b2fd8d00e6d7e153.

--- hello.py
+++ hello.py
@@ -1,9 +1,8 @@
 // For command line git tutorial
 // First: socal repository modification
 // Second: GitHub modification
-// Third: git commit --amend
+
 print("Hello World")
 print("Tell Your World")
 print("Tell his World")
 print("Tell her World")
-print("Tell my World")
