5a5f1baac34733dbfcbbefb30088e7b7bbfe61ea
Jun Hong <hongj7794@gmail.com>
2018-11-18 15:52:21 +0900

    GitHub for git pull by Jun-Hong from MORAI!

--- hello.py
+++ hello.py
@@ -1,8 +1,9 @@
 // For command line git tutorial
 // First: socal repository modification
 // Second: GitHub modification
-
+// Third: git commit --amend
 print("Hello World")
 print("Tell Your World")
 print("Tell his World")
 print("Tell her World")
+print("Tell my World")
